9d904bc0fd9457976f36141493eb89f12a70ddae
sangot <36930141+bernatvinyals@users.noreply.github.com>
2021-11-28 20:56:17 +0100

    Revert "Re"
    This reverts commit bda536d639a86c5363b935ff4120df75d90ae89e.

---
+++ Source/ControlledMAIN.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ControlledMAINTarget : TargetRules
+{
+	public ControlledMAINTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "ControlledMAIN" } );
+	}
+}
