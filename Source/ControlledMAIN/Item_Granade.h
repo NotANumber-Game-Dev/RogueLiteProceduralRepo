a54bbed32cda36a13ce6ea0b51f4064a73226724
BERNAT VIÑALS <bernat.vinals@epia.granollers>
2021-11-29 17:15:58 +0100

    QUICK FIX

---
+++ Source/ControlledMAIN/Item_Granade.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Item_Granade.generated.h"
+
+UCLASS()
+class CONTROLLEDMAIN_API UItem_Granade : public UClass
+{
+	GENERATED_BODY()
+};
