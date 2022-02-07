8f2f9368d4c70f4691be90810355c79d17cee602
bernat.vinals <bernat.vinals@E118-09>
2022-02-07 19:46:18 +0100

    WORKIN' Database
    UE4Editor_ControlledMAIN_0019!UDATABASE::GetNextQuest() [D:\RogueLiteProceduralRepo\Source\ControlledMAIN\DATABASE.cpp:40]
    UE4Editor_ControlledMAIN_0019!UDATABASE::execGetNextQuest() [D:\RogueLiteProceduralRepo\Intermediate\Build\Win64\UE4Editor\Inc\ControlledMAIN\DATABASE.gen.cpp:713]

--- Source/ControlledMAIN/DATABASE.cpp
+++ Source/ControlledMAIN/DATABASE.cpp
@@ -2,3 +2,79 @@
 
 
 #include "DATABASE.h"
+
+void UDATABASE::Start()
+{
+	for (int i = 0; i< DATABASE_QUEST_LIST.Num();i++)
+	{
+		for (int x = 0; x< DATABASE_QUEST_LIST[i].Quest_List.Num();x++)
+		{
+			DATABASE_QUEST_LIST[i].Quest_List[x].ID = ((1000*i)+x);
+		}
+	}
+}
+
+void UDATABASE::AddEnemiesKilledCounter()
+{
+	for (TPair<int32, int32>& pair : CounterEnemiesKilled)
+	{      
+		pair.Value++;  
+	} 
+}
+
+void UDATABASE::SetCounterForEnemiesKilled(TSubclassOf<AActor> Instigator, int Current_Quest, int Amount)
+{
+	for (int i = 0; i< DATABASE_QUEST_LIST.Num();i++)
+	{
+		if (DATABASE_QUEST_LIST[i].Actor_Assigned->GetClass() == Instigator->GetClass() )
+		{
+			CounterEnemiesKilled.Add(DATABASE_QUEST_LIST[i].Quest_List[Current_Quest].ID,Amount);
+		}
+	}
+}
+
+void UDATABASE::GetNextQuest(TSubclassOf<AActor> Instigator, bool& noMoreMisions)
+{
+	for (int i = 0; i< DATABASE_QUEST_LIST.Num();i++)
+	{
+		if (DATABASE_QUEST_LIST[i].Actor_Assigned->GetClass() == Instigator->GetClass() )
+		{
+			if (DATABASE_QUEST_LIST[i].Quest_List.Num() > DATABASE_QUEST_LIST[i].Mission_Index + 1)
+			{
+				DATABASE_QUEST_LIST[i].Mission_Index = DATABASE_QUEST_LIST[i].Mission_Index + 1;
+				noMoreMisions = false;
+				return;
+			}
+			else
+			{
+				noMoreMisions = true;
+				return;
+			}
+		}
+	}
+}
+
+void UDATABASE::SetStateQuest(TSubclassOf<AActor> Instigator, int QuestIndex, EQuest_State inState)
+{
+	for (int i = 0; i< DATABASE_QUEST_LIST.Num();i++)
+	{
+		if (DATABASE_QUEST_LIST[i].Actor_Assigned->GetClass() == Instigator->GetClass() )
+		{
+			DATABASE_QUEST_LIST[i].Quest_List[QuestIndex].State = inState;
+			if(GEngine)
+				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow,
+					*UEnum::GetDisplayValueAsText(DATABASE_QUEST_LIST[i].Quest_List[QuestIndex].State).ToString());
+		}
+	}
+}
+
+void UDATABASE::CheckAndUpdateQuests()
+{
+	
+}
+
+void UDATABASE::SetObjectiveToComplete(FQuest& questToCheck, int IndexOfObjective)
+{
+	questToCheck.isCompleted = true;
+	questToCheck.Objectives[IndexOfObjective].IsCompleted = true;
+}
--- Source/ControlledMAIN/DATABASE.h
+++ Source/ControlledMAIN/DATABASE.h
@@ -7,35 +7,141 @@
 #include "UObject/Object.h"
 #include "DATABASE.generated.h"
 
+UENUM(BlueprintType,Blueprintable)
+enum EQuest_State
+{
+	Not_Grabbed,
+	Grabbed,
+	Complete
+};
 
-USTRUCT(BlueprintType)
+UENUM(BlueprintType,Blueprintable)
+enum EQuest_Story_Priority
+{
+	Main,
+	Secundary
+};
+
+UENUM(BlueprintType,Blueprintable)
+enum EQuest_Objective_Type
+{
+	RECOLECCTION,
+	INTERACT_WITH_OBJECT,
+	KILL_N_ENEMIES,
+	KILL_N_ENEMIES_SPECIFIC,
+	KILL_SPECIFIC_ENEMY
+};
+
+USTRUCT(BlueprintType,Blueprintable)
+struct FQuest_Objective
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "Is Completed"))
+	bool IsCompleted = false;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere,meta = (TitleProperty = "Type"))
+	TEnumAsByte<EQuest_Objective_Type> Type;
+	
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "UI Player Instruction"))
+	FText UI_Player_Instruction;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere,meta = (TitleProperty = "What to recolect"))
+	TSubclassOf<AActor> What_to_recollect; //RECOLECCTION Has to be BP_ITEM
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (TitleProperty = "What to recolect Amount"))
+	int32 What_to_recollect_Amount; //RECOLECCTION
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere,meta = (TitleProperty = "What to interact"))
+	TSubclassOf<AActor> What_to_Interact; //INTERACT_WITH_OBJECT Has to be BP_Quest_Interactuable
+	
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "What to kill"))
+	TSubclassOf<AActor> What_to_Kill; //KILL_N_ENEMIES_SPECIFIC Has to be BP_ENEMY
+	
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "What to kill Amount"))
+	int32 What_to_Kill_Amount; //KILL_N_ENEMIES
+
+};
+
+USTRUCT(BlueprintType,Blueprintable)
 struct FQuest
 {
 	GENERATED_BODY()
-	int ID;
+public:
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "isCompleted"))
+	bool isCompleted = false;
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "ID"))
+	int32 ID;
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "Title"))
 	FText Title;
+	
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "Description"))
 	FText Description;
-	//TODO STRUCT ARRAY OBJECTIVES
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "Objectives"))
+	TArray<FQuest_Objective> Objectives;
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "NPC Dialogues"))
 	TArray<FText> NPC_Dialogues;
-	//TODO Enum Priority
-	//TODO Enum Quest State
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere,meta = (TitleProperty = "Priority"))
+	TEnumAsByte <EQuest_Story_Priority> Priority;
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere, meta = (TitleProperty = "ID"))
+	TEnumAsByte <EQuest_State> State;
 };
 
-USTRUCT(BlueprintType)
+USTRUCT(BlueprintType,Blueprintable)
 struct FNPC_Quest_List
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (TitleProperty = "Assigned Actor"))
 	TSubclassOf<AActor> Actor_Assigned;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (TitleProperty = "Quest List"))
 	TArray<FQuest> Quest_List;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (TitleProperty = "Mission Index"))
+	int32 Mission_Index = -1;
 };
-
-
 
 /**
  * 
  */
-UCLASS()
+UCLASS(BlueprintType,Blueprintable)
 class CONTROLLEDMAIN_API UDATABASE : public UObject
 {
 	GENERATED_BODY()
+
+protected:
+	bool debug;
+	
+	TMap<int32,int32> CounterEnemiesKilled;
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere,meta = (TitleProperty = "Database Quest List"))
+	TArray<FNPC_Quest_List> DATABASE_QUEST_LIST;
+
+	UFUNCTION(BlueprintCallable)
+	void Start();
+	
+	///Adds 1 to all quests that are kill any Enemies
+	UFUNCTION(BlueprintCallable)
+	void AddEnemiesKilledCounter();
+
+	UFUNCTION(BlueprintCallable)
+	void SetCounterForEnemiesKilled(TSubclassOf<AActor> Instigator, int Current_Quest, int Amount = 0);
+
+	UFUNCTION(BlueprintCallable)
+	void GetNextQuest(TSubclassOf<AActor> Instigator, bool &noMoreMisions);
+
+	UFUNCTION(BlueprintCallable)
+	void SetStateQuest(TSubclassOf<AActor> Instigator,int QuestIndex, EQuest_State inState);
+	
+	
+	void CheckAndUpdateQuests();
+	void SetObjectiveToComplete(FQuest &questToCheck, int IndexOfObjective);
 };
